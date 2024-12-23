124b257c9f4a793ba6576900cd7f56aa5f708076
HwangSukHyeon <payback9488@gmail.com>
2024-12-23 16:04:32 +0900

    [proj]: 초기 세팅
    - SnapKit 추가
    - CodeBase 세팅

---
+++ NBC_W10-11-BookCart/NBC_W10-11-BookCart/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NBC_W10-11-BookCart
+//
+//  Created by 황석현 on 12/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
